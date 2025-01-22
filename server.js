48840f521090a3569af0c48a70fedb32ee8a25d3
samarthlawania <samarthlawania2002@gmail.com>
2025-01-22 18:01:20 +0530

    cors origin chnaged

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const app = express();
 const port = 5000;
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://rq-frontend.vercel.app/", credentials: true }));
 
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
